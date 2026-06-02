2eaaa6916620d23b555d30abf1972421fd12a553
Toyesh2907 <97665574+Toyesh2907@users.noreply.github.com>
2026-06-02 09:45:59 +0530

    Add: car-fleet - submission-7

---
+++ Data Structures & Algorithms/car-fleet/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+        
+    def create_turns_array(self, target: int, position: list[int], speed: list[int]) -> list[int]:
+        turns_array = []
+        for i in range(len(position)):
+            turns_array.append((target - position[i]) / speed[i])
+        return turns_array
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        array_len = len(position)
+        if array_len == 1:
+            return 1
+        turns_array = self.create_turns_array(target=target, position = position, speed = speed)
+        turns_reverse_sorted = [x for _, x in sorted(zip(position, turns_array), reverse=True)]
+        print(turns_reverse_sorted)
+        fleet = 1
+        max_time = -1
+        for i in range(array_len - 1):
+            max_time = max(turns_reverse_sorted[i], max_time)
+            if max_time >= turns_reverse_sorted[i + 1]:
+                continue
+            else:
+                fleet += 1
+            max_time = max(max_time, turns_reverse_sorted[i+1])
+        return fleet
