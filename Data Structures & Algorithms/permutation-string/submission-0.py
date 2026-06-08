db4909e31e2bc532de6240321971bc4d363e47dc
Toyesh2907 <97665574+Toyesh2907@users.noreply.github.com>
2026-06-08 11:20:55 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        freq1 = defaultdict(int)
+        freq2 = defaultdict(int)
+
+        for c in s1:
+            freq1[c] += 1
+
+        for c in s2[:len(s1)]:
+            freq2[c] += 1
+
+        i, j = 0, len(s1) - 1
+
+        while j < len(s2):
+            if freq1 == freq2:
+                return True
+
+            freq2[s2[i]] -= 1
+            if freq2[s2[i]] == 0:
+                del freq2[s2[i]]
+
+            i += 1
+            j += 1
+
+            if j < len(s2):
+                freq2[s2[j]] += 1
+
+        return False
